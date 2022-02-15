eb0f6526d85981fa51858255709e2ed71860a40d
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2022-02-15 18:35:20 +0100

    Implemented extraction of ground truth

--- src/main/java/CodeBlock.java
+++ src/main/java/CodeBlock.java
@@ -1,17 +1,17 @@
 import org.logicng.formulas.Formula;
 
-import java.util.Objects;
-
 
 public final class CodeBlock {
     private final int start;
     private final int end;
-    private final Formula condition;
+    private final Formula blockCondition;
+    private final Formula presenceCondition;
 
-    private CodeBlock(int start, int end, Formula condition) {
-        this.start = start;
+    private CodeBlock(int start, int end, Formula blockCondition, Formula presenceCondition) {
+        this.start = start+1;
         this.end = end;
-        this.condition = condition;
+        this.blockCondition = blockCondition;
+        this.presenceCondition = presenceCondition;
     }
 
     public int start() {
@@ -22,56 +22,24 @@ public final class CodeBlock {
         return end;
     }
 
-    public Formula condition() {
-        return condition;
+    public Formula blockCondition() {
+        return blockCondition;
     }
 
-    public static UnderConstruction startBuildingBlock(int startLine, Formula condition) {
-        return new UnderConstruction(startLine, condition);
+    public Formula getPresenceCondition() {
+        return presenceCondition;
+    }
+
+    public static UnderConstruction startBuildingBlock(int startLine, Formula blockCondition, Formula presenceCondition) {
+        return new UnderConstruction(startLine, blockCondition, presenceCondition);
     }
 
     public static CodeBlock finishBlock(final UnderConstruction unfinishedBlock, final int endLine) {
-        return new CodeBlock(unfinishedBlock.start, endLine, unfinishedBlock.condition);
+        return new CodeBlock(unfinishedBlock.start, endLine, unfinishedBlock.blockCondition, unfinishedBlock.presenceCondition);
     }
 
-    public static final class UnderConstruction {
-        private final int start;
-        private final Formula condition;
-
-        private UnderConstruction(int start, Formula condition) {
-            this.start = start;
-            this.condition = condition;
-        }
-
-        public int start() {
-            return start;
-        }
-
-        public Formula condition() {
-            return condition;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == this) return true;
-            if (obj == null || obj.getClass() != this.getClass()) return false;
-            var that = (UnderConstruction) obj;
-            return this.start == that.start &&
-                    Objects.equals(this.condition, that.condition);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(start, condition);
-        }
-
-        @Override
-        public String toString() {
-            return "UnderConstruction[" +
-                    "start=" + start + ", " +
-                    "condition=" + condition + ']';
-        }
-
+    public record UnderConstruction(int start, Formula blockCondition,
+                                    Formula presenceCondition) {
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,13 @@ import org.logicng.formulas.FormulaFactory;
 import org.logicng.io.parsers.ParserException;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /*
@@ -20,6 +23,7 @@ We have to account for
  */
 public class Main {
     private static final Path workDir = Path.of("/home/alex/develop/bachelor_projects/BA-Angelina/argouml-spl-benchmark/argouml-app/src/org/argouml/");
+    private static final Path outputFile = Path.of("result/argouml.spl.csv");
     private static final FormulaFactory factory = new FormulaFactory();
 
 
@@ -30,8 +34,17 @@ public class Main {
 //        Formula test = factory.and(factory.or(factory.literal("A", false), factory.literal("B", true)), factory.or(factory.literal("C", false), factory.literal("D", true)));
 //        System.out.println(test);
         // Parse each file
-        final List<CodeBlock> blocks = new LinkedList<>();
-        javaFiles.forEach(f -> blocks.addAll(determineBlocks(f)));
+        List<String> csvLines = new LinkedList<>();
+        for (final Path path : javaFiles) {
+            final List<CodeBlock> blocks = determineBlocks(path);
+            for (CodeBlock block: blocks) {
+                csvLines.add(toCSVLine(path, block));
+            }
+        }
+        if (outputFile.getParent().toFile().mkdirs()) {
+            System.out.println("Created directory " + outputFile.getParent());
+        }
+        Files.write(outputFile, csvLines);
     }
 
     public static List<Path> searchJavaFiles(final Path root) throws IOException {
@@ -43,39 +56,57 @@ public class Main {
     }
 
     public static List<CodeBlock> determineBlocks(final Path pathToFile) {
-        final List<String> lines;
+        List<String> lines;
         try {
-            lines = Files.readAllLines(pathToFile);
+            lines = Files.readAllLines(pathToFile, StandardCharsets.UTF_8);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            try {
+                lines = Files.readAllLines(pathToFile, StandardCharsets.ISO_8859_1);
+            } catch (IOException ex) {
+                System.err.println("Was not able to read " + pathToFile);
+                throw new RuntimeException(e);
+            }
+
         }
         final List<CodeBlock> blocks = new LinkedList<>();
         final LinkedList<CodeBlock.UnderConstruction> unfinishedBlocks = new LinkedList<>();
+        final LinkedList<Formula> nestedFormulas = new LinkedList<>();
 
-        int blockStart = 0;
         for (int i = 0; i < lines.size(); i++) {
             final String line = lines.get(i);
             if (blockStart(line) || i == 0) {
                 // Add a new unfinished block to the stack
-                unfinishedBlocks.add(CodeBlock.startBuildingBlock(blockStart, toFormula(line)));
+                final Formula formula = toFormula(line);
+                nestedFormulas.add(formula);
+                unfinishedBlocks.add(CodeBlock.startBuildingBlock(i, formula, toPC(nestedFormulas)));
             } else if (blockSwitch(line)) {
                 // Convert the last started block to a completed block
-                CodeBlock finishedBlock = CodeBlock.finishBlock(unfinishedBlocks.pop(), i);
+                if (unfinishedBlocks.isEmpty()) {
+                    throw new IllegalStateException("No unfinished blocks remain in " + pathToFile + " at line " + i);
+                }
+                CodeBlock finishedBlock = CodeBlock.finishBlock(Objects.requireNonNull(unfinishedBlocks.pollLast()), i);
                 blocks.add(finishedBlock);
+                nestedFormulas.pollLast();
                 // Start a new block with the negated Formula
-                unfinishedBlocks.add(CodeBlock.startBuildingBlock(i, finishedBlock.condition().negate()));
+                final Formula formula = finishedBlock.blockCondition().negate();
+                nestedFormulas.add(formula);
+                unfinishedBlocks.add(CodeBlock.startBuildingBlock(i, formula, toPC(nestedFormulas)));
             } else if (blockEnd(line)) {
                 // Convert the last started block to a completed block
-                blocks.add(CodeBlock.finishBlock(unfinishedBlocks.pop(), i));
+                if (unfinishedBlocks.isEmpty()) {
+                    throw new IllegalStateException("No unfinished blocks remain in " + pathToFile + " at line " + i);
+                }
+                blocks.add(CodeBlock.finishBlock(Objects.requireNonNull(unfinishedBlocks.pollLast()), i));
+                nestedFormulas.pollLast();
             }
         }
 
-        // Finish the remaining block that must have condition 'true'
+        // Finish the remaining block that must have blockCondition 'true'
         if (!unfinishedBlocks.isEmpty()) {
-            CodeBlock finishedBlock = CodeBlock.finishBlock(unfinishedBlocks.pop(), lines.size()-1);
+            CodeBlock finishedBlock = CodeBlock.finishBlock(unfinishedBlocks.pollLast(), lines.size());
             blocks.add(finishedBlock);
-            if (!finishedBlock.condition().evaluate(new Assignment())) {
-                throw new IllegalStateException("Remaining Block does not have the condition 'true'");
+            if (!finishedBlock.blockCondition().evaluate(new Assignment())) {
+                throw new IllegalStateException("Remaining Block does not have the blockCondition 'true'");
             }
         }
 
@@ -83,19 +114,20 @@ public class Main {
             throw new IllegalStateException("Blocks remain unfinished.");
         }
 
+        blocks.sort(Comparator.comparingInt(CodeBlock::start));
         return blocks;
     }
 
     public static boolean blockStart(final String line) {
-        return line.trim().startsWith("//#if defined");
+        return line.contains("//#if defined");
     }
 
     public static boolean blockSwitch(final String line) {
-        return line.trim().startsWith("//#else");
+        return line.contains("//#else");
     }
 
     public static boolean blockEnd(final String line) {
-        return line.trim().startsWith("//#endif");
+        return line.contains("//#endif");
     }
 
     public static Formula toFormula(String line) {
@@ -104,11 +136,14 @@ public class Main {
         }
         // Trim whitespace
         line = line.trim();
-        // Cut everything of till the first condition
-        line = line.substring(line.indexOf("defined"));
+        // Cut everything of till the first blockCondition
+        line = line.substring(line.indexOf("defined("));
         // Remove "defined(" and ")"
         line = line.replaceAll("defined\\(", "");
         line = line.replaceAll("\\)", "");
+        // Replace 'and' and 'or'
+        line = line.replaceAll("and", "&");
+        line = line.replaceAll("or", "|");
         try {
             return factory.parse(line);
         } catch (ParserException e) {
@@ -116,4 +151,16 @@ public class Main {
         }
     }
 
+    public static Formula toPC(List<Formula> nestedFormulas) {
+        return factory.and(nestedFormulas);
+    }
+
+    public static String toCSVLine(Path path, CodeBlock codeBlock) {
+        return String.format("%s;%s;%s;%s;%d;%d", path.toString(), "true", fixCondition(codeBlock.blockCondition().toString()), fixCondition(codeBlock.getPresenceCondition().toString()), codeBlock.start(), codeBlock.end());
+    }
+
+    public static String fixCondition(String condition) {
+        condition = condition.replaceAll("&", "&&");
+        return condition.replaceAll("\\|", "||");
+    }
 }
